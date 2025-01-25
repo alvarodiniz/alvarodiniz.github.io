d59d4b068b119ad5422bc112b7a90f2018b7b50e
esdrasadiniz <esdras.pert@gmail.com>
2025-01-25 13:20:25 -0300

    correção menu mobile

--- js/menu-mobile.js
+++ js/menu-mobile.js
@@ -3,17 +3,28 @@ const openMenuButton = document.querySelector('.open-menu');
 const closeMenuButton = document.querySelector('.close-menu');
 const mobileMenu = document.querySelector('.main-menu');
 
+// Função para verificar se está no modo mobile
+function isMobile() {
+  return window.innerWidth <= 1500; // Ajuste conforme seu breakpoint do CSS
+}
+
 // Função para abrir o menu
 function openMenu() {
-  mobileMenu.style.transform = 'translateX(-100%)'; // Move o menu para dentro da tela
+  if (isMobile()) {
+    mobileMenu.style.transform = 'translateX(-100%)'; // Move o menu para dentro da tela
+    mobileMenu.style.transition = 'transform 0.3s ease'; // Adiciona uma transição suave
+  }
 }
 
 // Função para fechar o menu
 function closeMenu() {
-  mobileMenu.style.transform = 'translateX(0%)'; // Move o menu para fora da tela
+  if (isMobile()) {
+    mobileMenu.style.transform = 'translateX(0%)'; // Move o menu para fora da tela
+    mobileMenu.style.transition = 'transform 0.3s ease'; // Adiciona uma transição suave
+  }
 }
 
-// Adicionando os eventos de clique
+// Adicionando os eventos de clique para abrir e fechar o menu
 openMenuButton.addEventListener('click', (e) => {
   e.preventDefault(); // Evita o comportamento padrão do link
   openMenu();
